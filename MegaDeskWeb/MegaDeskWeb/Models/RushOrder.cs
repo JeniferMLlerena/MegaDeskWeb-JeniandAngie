17c2a1231a4319b210b8798e960e42f7d43b89eb
Angie Garcia-Paz <agarpaz06@gmail.com>
2019-06-20 10:14:26 -0600

    rush order

--- MegaDeskWeb/MegaDeskWeb/Models/RushOrder.cs
+++ MegaDeskWeb/MegaDeskWeb/Models/RushOrder.cs
@@ -8,7 +8,9 @@ namespace MegaDeskWeb.Models
     public class RushOrder
     {
         public int RushOrderId { get; set; }
-        public string RushOrderType { get; set; }
-        public int RushOrderPrice { get; set; }
+        public string RushOrderType { get; set; }//name 3 day,5 days
+        public decimal RushOrderPriceLessThan1000 { get; set; }
+        public decimal RushOrderPrice1000To2000 { get; set; }
+        public decimal RushOrderPriceOver2000 { get; set; }
     }
 }
